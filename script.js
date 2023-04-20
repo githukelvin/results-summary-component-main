7e48d1fa75e47ef9c14d90cadf17d7ccb3a655ba
githukelvin <kelvingithu019@gmail.com>
2023-04-20 07:38:16 +0300

    build finished now

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let parentDiv = document.querySelector(".divs");
+const parentDiv = document.querySelector(".divs");
 
-fetch("data.json")
-  .then((response) => response.json())
-  .then((json) =>
-  // consoe.log(json)
-    json
-      .map(({category, score, icon}) => {
-        return (parentDiv.innerHTML += `
-        <div class="div ${category}">
-              <div><img src="${icon}" alt="image of ${category}">
-              <p>${category}</p></div>
-              <p class="par">${score}  <span>/ 100</span></p>
-            </div>
-        `);
-      })
-      .join("")
-  );
+async function fetchData() {
+  try {
+    const response = await fetch("data.json");
+    const data = await response.json();
 
+    const html = data
+      .map(
+        ({ category, score, icon }) => `
+      <div class="div ${category}">
+        <div>
+          <img src="${icon}" alt="image of ${category}">
+          <p>${category}</p>
+        </div>
+        <p class="par">${score} <span>/ 100</span></p>
+      </div>
+    `
+      )
+      .join("");
 
+    parentDiv.innerHTML = html;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+fetchData();
